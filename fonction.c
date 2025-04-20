ca8515ab6dea7dffec31fd7d88c9635ac2727a50
aranney17 <aranney.sivakumar@etu.cyu.fr>
2025-04-20 18:40:10 +0200

    Update fonction.c

--- fonction.c
+++ fonction.c
@@ -2,11 +2,13 @@
 
 //weekly time to clean shelters
 int day_clean(Animal* tab, int size){
-  // Local
-  clean_time=0;
+  // Locals
+  int clean_time=0;
+  int h=-1;
+  int m=-1;
 
   //Check parameters
-  if (tab == NULL || size<=0){
+  if (tab == NULL ||  size<=0){
     printf("allocation échoué ou taille incohérente\n");
     exit(1);
   }
@@ -16,16 +18,29 @@ int day_clean(Animal* tab, int size){
     if (tab[i].species==1 || tab[i].species==2){
       clean_time+=90;
     }
-    else if (tab[i]==3){
+    else if (tab[i].species==3){
       clean_time+=185;
     }
-    else if (tab[i]==1){
+    else if (tab[i].species==1){
       clean_time+=55;
     }
     else{
       clean_time+=14;
     }
   }
-  //end of process
-  return clean_time;
+
+  //Check clean_time value
+  if (clean_time%60==0){
+    h=clean_time/60;
+    printf("Il faudra prévoir %dh de charge de travail cette semaine pour nettoyer les abris de chaque animal.\n",h);
+  }
+  else if (clean_time>60){
+    h=clean_time/60;
+    m=clean_time%60;
+    printf("Il faudra prévoir %dh%d de charge de tavail cette semaine pour nettoyer les abris de chaque animal.\n",h,m);
+  }
+  else{
+    m=clean_time;
+    printf("Il faudra prévoir %dmin de charge de travail cette semaine pour nettoyer les abris de chaque animal.\n",m);
+  }
 }
