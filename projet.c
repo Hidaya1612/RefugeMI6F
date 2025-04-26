122ccfbfb71d65d5a56a57a3fbd963c0d84fdc35
aranney17 <aranney.sivakumar@etu.cyu.fr>
2025-04-26 19:33:33 +0200

    Update projet.c

--- projet.c
+++ projet.c
@@ -1,7 +1,25 @@
-#include <stdio.h>
 #include "refuge.h"
 
 int main(){
+  
+  Animal* tab=NULL;
+  FILE* f1=NULL;
+  FILE* f2=NULL;
+
+  tab=malloc(TAILLE*sizeof(Animal));
+  if (tab==NULL){
+    printf("Allocation échouée\n");
+    exit(1);
+  }
+
+  f1=fopen("liste_num_identification.txt","r+");
+  f2=fopen("liste_animaux.txt","r+");
+  if (f1==NULL || f2==NULL){
+    printf("Ouverture des fichiers impossible\n");
+    exit(1);
+  }
+
+  
   afficherMenu();
   return 0;
 }
