17cb57db46b0118fc6b1ee0117c4e294931a07bb
Imane Bateoui <e-ibateoui1@etu.cyu.fr>
2025-05-05 23:41:44 +0200

    Update refuge.h

--- refuge.h
+++ refuge.h
@@ -27,12 +27,12 @@ void ajouter_Animal(Animal* tab, int taille, int* nb_animaux);
 void adoption_animal(Animal* tab, int taille, int* nb_animaux);
 void day_clean(Animal* tab, int size);
 void afficherMenu();
-Animal* rechercherAnimaux(Animal* a, int* nb_animaux);
+Animal* rechercherAnimaux(Animal tab[], int nb_animaux);
 Animal* rechercheparNom(char* nomRecherche, Animal animaux[], int* nb_animaux);
 Animal* rechercheparEspece(Species s, Animal animaux[], int* nb_animaux);
 Animal* rechercheparAge(int ageType, Animal animaux[], int* nb_animaux);
 void extremums(Animal* tab, int nb_animaux, int* pMin, int* pMax);
-void inv_age_asc(Animal* tab, int nb_animaux, int* pMin, int* pMax);
+void inv_age_asc(Animal* tab, int nb_animaux);
 
 
 
