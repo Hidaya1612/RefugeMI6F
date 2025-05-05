c1638b66b4ca9c2944ce79800cb3ed16c40d6e21
Hidaya Naloufi <e-hnaloufi@etu.cyu.fr>
2025-05-05 10:56:55 +0200

    Update refuge.h

--- refuge.h
+++ refuge.h
@@ -23,7 +23,7 @@ typedef struct {
 //Date constructeur();
 int stockage_animaux(Animal* tab, int taille);
 void  corrigeNom(Animal a)
-Animal ajouter_Animal();
+void ajouter_Animal(Animal* tab, int taille, int* nb_animaux);
 void adoption_animal(Animal* tab, int taille, int* nb_animaux);
 void day_clean(Animal* tab, int size);
 void afficherMenu();
