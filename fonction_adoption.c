3c294492bf41c11f80a21dff114876eed20807b0
Hidaya Naloufi <e-hnaloufi@etu.cyu.fr>
2025-05-09 15:06:40 +0200

    Update fonction_adoption.c

--- fonction_adoption.c
+++ fonction_adoption.c
@@ -1,29 +1,29 @@
 #include "refuge.h"
 
 void adoption_animal(Animal* tab, int taille, int* pnb_animaux){
-    
+
     int id=-1;
     int nb_animaux=*pnb_animaux;
-    
+
     if (tab == NULL || pnb_animaux == NULL ||  taille<=0 || nb_animaux<0 || nb_animaux>taille){
-        printf("allocation échoué ou taille/nombre animaux incohérente\n");
+        printf("allocation echouee ou taille/nombre animaux incoherente\n");
         exit(1);
     }
-    
+
     if (nb_animaux==0){
         printf("Il n'est plus possible d'adopter un animal dans ce refuge.\n");
     }
     else{
-        printf("Saisir le numéro d'identification de l'animal que vous voulez adopter:\n");
-         scan("%d",&id);
+        printf("Saisir le numero d'identification de l'animal que vous voulez adopter:\n");
+        scan("%d",&id);
         while(id<0){
             printf("Mauvaise saisie du numéro d'identification!\n");
             scan("%d",&id);
         }
         int test=0;
         for (int i=0;i<nb_animaux;i++){
-            if(tab[i].identification_number==id){
-                tab[i].identification_number=0;
+            if(tab[i].num_identification==id){
+                tab[i].num_identification=0;
                 test++;
             }
         }
@@ -32,14 +32,14 @@ void adoption_animal(Animal* tab, int taille, int* pnb_animaux){
         }
         else{
             FILE* fichier=NULL;
-            fichier=fopen("Animaux/liste_animaux.txt","w+");
+            fichier=fopen("liste_animaux.txt","w+");
             if (fichier==NULL ){
                 printf("Ouverture du fichier impossible\n");
                 exit(1);
             }
             for (int i=0;i<nb_animaux;i++){
-                if (tab[i].identification_number!=0){
-                    fprintf(fichier,"%d %s %d %d %f",tab[i].identification_number,tab[i].name,tab[i].species,tab[i].year_of_birth,tab[i].weight);
+                if (tab[i].num_identification!=0){
+                    fprintf(fichier,"%d %s %d %d %f",tab[i].num_identification,tab[i].nom,tab[i].espece,tab[i].annee_de_naissance,tab[i].poids);
                     if (strlen(tab[i].description)!=0){
                         fprintf(fichier," %s;\n",tab[i].description);
                     }
@@ -51,6 +51,7 @@ void adoption_animal(Animal* tab, int taille, int* pnb_animaux){
             rewind(fichier);
             fclose(fichier);
             *pnb_animaux=stockage_animaux(tab,taille);
+            printf("L'animal avec le numero d'identification %d a bien ete adopte.\n", id);
         }
     }
     afficherMenu(tab, TAILLE, *pnb_animaux);
