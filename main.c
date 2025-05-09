a776ed9dd0b97ea6199ab963a8656d2a3c77302b
aranney17 <aranney.sivakumar@etu.cyu.fr>
2025-05-10 01:41:24 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,11 +1,13 @@
 #include "refuge.h"
 
 int main(){
-
+    
+    //Initialisation
     Animal tab_animaux[TAILLE];
     int nb_animaux = stockage_animaux(tab_animaux, TAILLE);
     
     printf("\n\nBienvenue dans le refuge blabla !\n\n");
+    //appel du menu
     afficherMenu(tab_animaux, TAILLE, nb_animaux);
     return 0;
 }
