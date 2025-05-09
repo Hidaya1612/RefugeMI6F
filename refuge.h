935fd1b28743682c5f6242df65c9dd025398ef1a
Hidaya Naloufi <e-hnaloufi@etu.cyu.fr>
2025-05-09 15:02:48 +0200

    Update refuge.h

--- refuge.h
+++ refuge.h
@@ -4,19 +4,19 @@
 #include <string.h>
 #define TAILLE 50
 
-// Enumeration species
+// Enumeration espèces
 typedef enum {
-  DOG, CAT, HAMSTER, OSTRICH
-} Species;
+    CHIEN, CHAT, HAMSTER, AUTRUCHE
+  } Espece;
 
 // Structure animal
 typedef struct {
-  int identification_number;
-  char* name;
-  Species species;
-  int year_of_birth;
-  float weight;
-  char* description;
+    int num_identification;
+    char* nom;
+    Espece espece;
+    int annee_de_naissance;
+    float poids;
+    char* description;
 } Animal;
 
 //Prototypes
@@ -26,14 +26,11 @@ void scan(char* mode, void* input);
 void corrigeNom(char mot[]);
 void ajouter_Animal(Animal* tab, int taille, int* pnb_animaux);
 void adoption_animal(Animal* tab, int taille, int* pnb_animaux);
-void day_clean(Animal* tab, int taille, int nb_animaux);
+void nettoyage(Animal* tab, int taille, int nb_animaux);
 void afficherMenu(Animal* tab, int taille, int nb_animaux);
 void rechercherAnimaux(Animal tab[], int nb_animaux);
 Animal* rechercheparNom(char* nomRecherche, Animal animaux[], int* nb_animaux);
-Animal* rechercheparEspece(Species s, Animal animaux[], int* nb_animaux);
+Animal* rechercheparEspece(Espece s, Animal animaux[], int* nb_animaux);
 Animal* rechercheparAge(int ageType, Animal animaux[], int* nb_animaux);
 void extremums(Animal* tab, int nb_animaux, int* pMin, int* pMax);
 void inv_age_asc(Animal* tab, int nb_animaux);
-
-
-
