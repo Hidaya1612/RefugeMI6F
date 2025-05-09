5f363b0d85f047549c0248b6c857bb20be0ebecc
Imane Bateoui <e-ibateoui1@etu.cyu.fr>
2025-05-09 16:48:47 +0200

    Add files via upload

--- main.c
+++ main.c
@@ -4,8 +4,8 @@ int main(){
 
     Animal tab_animaux[TAILLE];
     int nb_animaux = stockage_animaux(tab_animaux, TAILLE);
-
-
+    
+    printf("\n\nBienvenue dans le refuge blabla !\n\n");
     afficherMenu(tab_animaux, TAILLE, nb_animaux);
     return 0;
 }
