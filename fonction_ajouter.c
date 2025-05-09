38a8e2fadce9b66dc8305ab74ba21ae7e775e0c6
Hidaya Naloufi <e-hnaloufi@etu.cyu.fr>
2025-05-09 15:06:04 +0200

    Update fonction_ajouter.c

--- fonction_ajouter.c
+++ fonction_ajouter.c
@@ -1,6 +1,6 @@
 #include "refuge.h"
 
-void scan(char* mode, void* input){
+void scan(char* mode, void* input){ //vérifie que les scan soient bien positifs et corrects
     int a=scanf(mode, input);
     while(getchar()!='\n'){};
     if(a<=0){
@@ -9,16 +9,16 @@ void scan(char* mode, void* input){
     }
 }
 
-void  corrigeNom(char mot[]){
+void  corrigeNom(char mot[]){ //corrige les prénoms saisis par les utilisateurs
     // Vérification si le nom est vide
     if (mot[0] == '\0') {
         printf("Erreur sur le prénom!\n");
 	    exit(1);
     }
-    
+
     for (int i = 0; mot[i] != '\0'; i++) {
         if ((mot[i] < 'a' || mot[i] > 'z') && (mot[i] < 'A' || mot[i] > 'Z')) {
-                printf("Erreur : Le nom contient un caractère non alphabétique : '%c'\n", mot[i]);
+                printf("Erreur : Le nom contient un caractere non alphabetique : '%c'\n", mot[i]);
                 exit(1);
         }
     }
@@ -39,15 +39,15 @@ void  corrigeNom(char mot[]){
 }
 
 
-void ajouter_Animal(Animal* tab, int taille, int* pnb_animaux){
-    
+void ajouter_Animal(Animal* tab, int taille, int* pnb_animaux){ //ajoute un animal au refuge
+
     int nb_animaux=*pnb_animaux;
-    
+
     if (tab == NULL || pnb_animaux == NULL || taille<=0 || nb_animaux<0 || nb_animaux>taille){
         printf("allocation échoué ou taille/nombre animaux incohérente\n");
         exit(1);
     }
-    
+
     if (nb_animaux==50){
         printf ("Le refuge ne prend plus d'animaux.\n");
     }
@@ -56,56 +56,56 @@ void ajouter_Animal(Animal* tab, int taille, int* pnb_animaux){
         char temporaire_descriptif[100];
         int choix_descriptif;
         int tmp=-1;
-        tab[nb_animaux].identification_number=tab[nb_animaux-1].identification_number+1;
-        
+        tab[nb_animaux].num_identification=tab[nb_animaux-1].num_identification+1;
+
         // Saisie des informations de l'animal
-        
+
         printf("Veuillez saisir le nom de l'animal : \n");
         scan("%s",temporaire_nom);
-	    
-        tab[nb_animaux].name=NULL;
-        tab[nb_animaux].name=malloc(strlen(temporaire_nom)*(sizeof(char)+1));
-        if(tab[nb_animaux].name==NULL){
+
+        tab[nb_animaux].nom=NULL;
+        tab[nb_animaux].nom=malloc(strlen(temporaire_nom)*(sizeof(char)+1));
+        if(tab[nb_animaux].nom==NULL){
             printf("Allocation réalisée pour le prénom de l'animal échouée\n");
             exit(1);
         }
-        tab[nb_animaux].name=temporaire_nom;
-        corrigeNom(tab[nb_animaux].name);
+        tab[nb_animaux].nom=temporaire_nom;
+        corrigeNom(tab[nb_animaux].nom);
         //printf("%s\n",tab[nb_animaux].name);
-        
+
         printf("Veuillez saisir l'espece de l'animal :\n 0 pour chien\n 1 pour chat\n 2 pour hamster\n 3 pour autruche\n");
 	scan("%d",&tmp);
 	while(tmp < 0 || tmp > 3){
-		printf("Erreur de la saisie de l'espece! \n");    
+		printf("Erreur de la saisie de l'espece! \n");
 		scan("%d",&tmp);
 	}
-        
-        tab[nb_animaux].species=tmp;
+
+        tab[nb_animaux].espece=tmp;
         //printf("%d\n",tab[nb_animaux].species);
-        
+
         printf("Veuillez saisir le poids de l'animal: \n");
-	scan("%f", &tab[nb_animaux].weight);
-	while(tab[nb_animaux].weight < 0 || tab[nb_animaux].weight > 150){
-		printf("Erreur de la saisie du poids! \n");    
-		scan("%f", &tab[nb_animaux].weight);
+	scan("%f", &tab[nb_animaux].poids);
+	while(tab[nb_animaux].poids < 0 || tab[nb_animaux].poids > 150){
+		printf("Erreur de la saisie du poids! \n");
+		scan("%f", &tab[nb_animaux].poids);
 	}
-       
+
         //printf("%f",tab[nb_animaux].weight);
-    
+
         printf("Veuillez saisir l'année de naissance de l'animal: \n");
-	scan("%d", &tab[nb_animaux].year_of_birth);
-	while(tab[nb_animaux].year_of_birth<1980 || tab[nb_animaux].year_of_birth>2025){
-		printf("Erreur de la saisie de l'annee de naissance! \n");    
-		scan("%d", &tab[nb_animaux].year_of_birth);
+	scan("%d", &tab[nb_animaux].annee_de_naissance);
+	while(tab[nb_animaux].annee_de_naissance<1980 || tab[nb_animaux].annee_de_naissance>2025){
+		printf("Erreur de la saisie de l'annee de naissance! \n");
+		scan("%d", &tab[nb_animaux].annee_de_naissance);
 	}
-       
-        
+
+
         //printf("%d",tab[nb_animaux].year_of_birth);
-        
+
         printf("Voulez-vous ajouter une description sur l'animal:\n tapez 1 pour oui sinon tapez 2\n");
 	scan("%d",&choix_descriptif);
 	while(choix_descriptif<1 || choix_descriptif>2){
-		printf("Erreur de la saisie du choix pour ajouter un descriptif à l'animal!! \n");    
+		printf("Erreur de la saisie du choix pour ajouter un descriptif à l'animal!! \n");
 		scan("%d",&choix_descriptif);
 	}
         tab[nb_animaux].description=NULL;
@@ -124,15 +124,15 @@ void ajouter_Animal(Animal* tab, int taille, int* pnb_animaux){
             tab[nb_animaux].description=malloc(1);
             tab[nb_animaux].description[0]='\0';
         }
-        
+
         FILE* fichier=NULL;
-        fichier=fopen("Animaux/liste_animaux.txt","a+");
+        fichier=fopen("liste_animaux.txt","a+");
         if (fichier==NULL ){
             printf("Ouverture du fichier impossible\n");
             exit(1);
         }
-        
-        fprintf(fichier,"%d %s %d %d %f",tab[nb_animaux].identification_number,tab[nb_animaux].name,tab[nb_animaux].species,tab[nb_animaux].year_of_birth,tab[nb_animaux].weight);
+
+        fprintf(fichier,"%d %s %d %d %f",tab[nb_animaux].num_identification,tab[nb_animaux].nom,tab[nb_animaux].espece,tab[nb_animaux].annee_de_naissance,tab[nb_animaux].poids);
         if (strlen(tab[nb_animaux].description)!=0){
             fprintf(fichier," %s;\n",tab[nb_animaux].description);
         }
@@ -142,7 +142,8 @@ void ajouter_Animal(Animal* tab, int taille, int* pnb_animaux){
         rewind(fichier);
         fclose(fichier);
         *pnb_animaux=stockage_animaux(tab,taille);;
-          
+
     }
+    printf("L'animal %s a bien ete ajoute au refuge.\n", tab[nb_animaux].nom);
     afficherMenu(tab, TAILLE, *pnb_animaux);
 }
