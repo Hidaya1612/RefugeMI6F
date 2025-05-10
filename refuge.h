a578bda22d880359511e8b1e564f1bf57e3808dd
Imane Bateoui <e-ibateoui1@etu.cyu.fr>
2025-05-10 14:38:43 +0200

    Update refuge.h

--- refuge.h
+++ refuge.h
@@ -20,16 +20,18 @@ typedef struct {
 } Animal;
 
 //Prototypes
-int stockage_animaux(Animal* tab, int taille);
+
 void scan(char* mode, void* input);
 int corrigeNom(char mot[]);
+int stockage_animaux(Animal* tab, int taille);
 void ajouter_Animal(Animal* tab, int taille, int* pnb_animaux);
 void adoption_animal(Animal* tab, int taille, int* pnb_animaux);
-void nettoyage(Animal* tab, int taille, int nb_animaux);
 void afficherMenu(Animal* tab, int taille, int nb_animaux);
-void rechercherAnimaux(Animal tab[], int nb_animaux);
 Animal* rechercheparNom(char* nomRecherche, Animal animaux[], int* nb_animaux);
 Animal* rechercheparEspece(Espece s, Animal animaux[], int* nb_animaux);
 Animal* rechercheparAge(int ageType, Animal animaux[], int* nb_animaux);
+void afficheAnimaux(Animal tab[], int nb_animaux);
+void rechercherAnimaux(Animal tab[], int nb_animaux);
 void extremums(Animal* tab, int nb_animaux, int* pMin, int* pMax);
 void inv_age_asc(Animal* tab, int nb_animaux);
+void nettoyage(Animal* tab, int taille, int nb_animaux);
