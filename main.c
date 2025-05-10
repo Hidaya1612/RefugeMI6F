2e1bd0f657f4d52a0abb7cf8cfdb6c0634df0425
Hidaya Naloufi <e-hnaloufi@etu.cyu.fr>
2025-05-10 13:12:37 +0200

    Update main.c

--- main.c
+++ main.c
@@ -6,7 +6,7 @@ int main(){
     Animal tab_animaux[TAILLE];
     int nb_animaux = stockage_animaux(tab_animaux, TAILLE);
     
-    printf("\n\nBienvenue dans le refuge blabla !\n\n");
+    printf("\n\nBienvenue dans le refuge AdoptMe !\n\n");
     //appel du menu
     afficherMenu(tab_animaux, TAILLE, nb_animaux);
     return 0;
