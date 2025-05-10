4752a52f94a75e275c369070164b1867c6cab979
Imane Bateoui <e-ibateoui1@etu.cyu.fr>
2025-05-10 14:36:50 +0200

    Update menu.c

--- menu.c
+++ menu.c
@@ -4,16 +4,17 @@
 void afficherMenu(Animal* tab_animaux, int taille, int nb_animaux){
     int choix;
     printf("\n**************MENU PRINCIPAL**************\n");
-    printf("1. Rechercher un animal\n");
-    printf("2. Ajouter un animal qui vous a ete confie\n");
-    printf("3. Adopter un animal\n");
-    printf("4. Inventaire du refuge (par age)\n");
-    printf("5. Gestion du nettoyage hebdomadaire\n");
-    printf("6. Quitter\n");
+    printf("1. Afficher les animaux du refuge\n");
+    printf("2. Rechercher un animal\n");
+    printf("3. Ajouter un animal qui vous a ete confie\n");
+    printf("4. Adopter un animal\n");
+    printf("5. Inventaire du refuge (par age)\n");
+    printf("6. Gestion du nettoyage hebdomadaire\n");
+    printf("7. Quitter\n");
 
     printf("Votre choix : ");//demande à l'utilsateur l'action qu'il souhaite effectuer
     scan("%d", &choix);
-    while(choix < 0 || choix > 6){
+    while(choix < 0 || choix > 7){
         printf("mauvaise saisie du choix.\n");
         printf("ressaisir votre choix\n");
         scan("%d", &choix);
@@ -22,24 +23,29 @@ void afficherMenu(Animal* tab_animaux, int taille, int nb_animaux){
     //appel des fonctions par rapport à la saisie
     switch (choix) {
         case 1:
-            rechercherAnimaux(tab_animaux, nb_animaux);
+            afficheAnimaux(tab_animaux, nb_animaux);
+            
         afficherMenu(tab_animaux, TAILLE, nb_animaux);
         break;
         case 2:
+            rechercherAnimaux(tab_animaux, nb_animaux);
+            afficherMenu(tab_animaux, TAILLE, nb_animaux);
+        break;
+        case 3:
             ajouter_Animal(tab_animaux,TAILLE,&nb_animaux);
 
         break;
-        case 3:
+        case 4:
             adoption_animal(tab_animaux,TAILLE,&nb_animaux);
         break;
-        case 4:
+        case 5:
             inv_age_asc(tab_animaux, nb_animaux);
         break;
 
-        case 5:
+        case 6:
             nettoyage(tab_animaux,TAILLE,nb_animaux);
         break;
-        case 6:
+        case 7:
             printf("Au revoir !\n");
         break;
     }
